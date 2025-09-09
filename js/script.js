c6daa3e4bf8c0fccc2a61c23eed703ea12910574
Your Name <you@example.com>
2025-09-09 17:56:08 +0500

    finish project

--- js/script.js
+++ js/script.js
@@ -4,10 +4,8 @@ let a = 18645899348573498856874567456745689
 let b = 55567845734757
 let c = 1220000
 
-if ( a == b || a == c || b == c){
-    console.log("teng");
-}
- else if ( a > b && a > c){
+
+  if ( a > b && a > c){
    console.log("a");
 } else if (b > c){
     console.log("b");
