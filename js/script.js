f873ca619cf0634f354c4bd17f111a5ba37a0cbd
Your Name <you@example.com>
2025-09-09 16:19:37 +0500

    finsh projekt

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
+  
+
+let a = 18645899348573498856874567456745689
+let b = 55567845734757
+let c = 1220000
+
+if ( a == b || a == c || b == c){
+    console.log("teng");
+}
+ else if ( a > b && a > c){
+   console.log("a");
+} else if (b > c){
+    console.log("b");
+}else{
+    console.log("c");
+}
 
 
-let c = '134'; 
-let d = 'eebvehwfjewh';
-let f = c; 
+    
+    
 
-c = d;
-d = f;
-
-console.log(c);
