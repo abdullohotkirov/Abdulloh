1efc99138a076b651d8aaac3c823b98ee6cd7cfc
Your Name <you@example.com>
2025-09-13 18:07:20 +0500

    finsh

--- js/script.js
+++ js/script.js
@@ -1,15 +1,64 @@
-  
-
-let a = 18645899348573498856874567456745689
-let b = 55567845734757
-let c = 1220000
 
 
-  if ( a > b && a > c){
-   console.log("a");
-} else if (b > c){
-    console.log("b");
-}else{
-    console.log("c");
+let id = 0
+
+switch (id) {
+    case 1:
+        console.log("yanvar")
+        break;
+
+     case 2:
+     console.log("fevral")
+     break;
+            
+
+     case 3:
+     console.log("mart")
+     break;
+
+
+     case 4:
+     console.log("aprel")
+     break;
+
+     case 5:
+     console.log("may")
+     break;
+
+
+     case 6:
+     console.log("iyun")
+     break;
+
+
+     case 7:
+     console.log("iyul")
+     break;
+
+
+     case 8:
+     console.log("avgust")
+     break;
+
+
+     case 9:
+     console.log("sentabr")
+     break;
+
+
+     case 10:
+     console.log("oktabr")
+     break;
+
+     case 11:
+     console.log("noyab")
+     break;
+
+     case 12:
+     console.log("dekabr")
+     break;
+
+    default:
+        console.log("kechirasiz bunday oy mavjud emas")
+        break;
 }
-
