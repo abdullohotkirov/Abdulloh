3a643bfe4e0dc5a03908258ee16ecf883ed02415
Your Name <you@example.com>
2025-09-21 21:29:07 +0500

    finshd

--- js/script.js
+++ js/script.js
@@ -1,64 +1,17 @@
+let str = "hello"
+
+let newstr = ""
+let num = 0
 
 
-let id = 0
-
-switch (id) {
-    case 1:
-        console.log("yanvar")
-        break;
-
-     case 2:
-     console.log("fevral")
-     break;
-            
-
-     case 3:
-     console.log("mart")
-     break;
-
-
-     case 4:
-     console.log("aprel")
-     break;
-
-     case 5:
-     console.log("may")
-     break;
-
-
-     case 6:
-     console.log("iyun")
-     break;
-
-
-     case 7:
-     console.log("iyul")
-     break;
-
-
-     case 8:
-     console.log("avgust")
-     break;
-
-
-     case 9:
-     console.log("sentabr")
-     break;
-
-
-     case 10:
-     console.log("oktabr")
-     break;
-
-     case 11:
-     console.log("noyab")
-     break;
-
-     case 12:
-     console.log("dekabr")
-     break;
-
-    default:
-        console.log("kechirasiz bunday oy mavjud emas")
-        break;
+for(let i = 0; i < str.length;i++){
+    if(str[i] == "a" || str[i] == "e" || str[i] == "u" || str[i] == "i" || str[i] == "o" || str[i] == "o'"){
+        newstr += str[i]
+    
+    } 
+    else{
+        num++
+    }
 }
+
+console.log( num);
